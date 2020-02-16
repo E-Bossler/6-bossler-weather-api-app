c6be4a2a757cf26da425b3e7332916246e4c6a76
Eric Bossler <eric.bossler@gmail.com>
2020-02-15 17:47:59 -0800

    finished MVP

--- assets/script/script.js
+++ assets/script/script.js
@@ -6,41 +6,39 @@ const dayZeroIcon = document.querySelector('#dayZeroIcon');
 const dayZeroTemp = document.querySelector('#dayZeroTemp');
 const dayZeroHumidity = document.querySelector('#dayZeroHumidity');
 const dayZeroWindSpeed = document.querySelector('#dayZeroWindSpeed');
-const dayZeroUVIndex = document.querySelector('#dayZeroUVIndex');
 
 const dayOneIcon = document.querySelector('#dayOneIcon');
 const dayOneTemp = document.querySelector('#dayOneTemp');
 const dayOneHumidity = document.querySelector('#dayOneHumidity');
 const dayOneWindSpeed = document.querySelector('#dayOneWindSpeed');
-const dayOneUVIndex = document.querySelector('#dayOneUVIndex');
 
 const dayTwoDate = document.querySelector('#dayTwoDate');
 const dayTwoIcon = document.querySelector('#dayTwoIcon');
 const dayTwoTemp = document.querySelector('#dayTwoTemp');
 const dayTwoHumidity = document.querySelector('#dayTwoHumidity');
 const dayTwoWindSpeed = document.querySelector('#dayTwoWindSpeed');
-const dayTwoUVIndex = document.querySelector('#dayTwoUVIndex');
 
 const dayThreeDate = document.querySelector('#dayThreeDate');
 const dayThreeIcon = document.querySelector('#dayThreeIcon');
 const dayThreeTemp = document.querySelector('#dayThreeTemp');
 const dayThreeHumidity = document.querySelector('#dayThreeHumidity');
 const dayThreeWindSpeed = document.querySelector('#dayThreeWindSpeed');
-const dayThreeUVIndex = document.querySelector('#dayThreeUVIndex');
 
 const dayFourDate = document.querySelector('#dayFourDate');
 const dayFourIcon = document.querySelector('#dayFourIcon');
 const dayFourTemp = document.querySelector('#dayFourTemp');
 const dayFourHumidity = document.querySelector('#dayFourHumidity');
 const dayFourWindSpeed = document.querySelector('#dayFourWindSpeed');
-const dayFourUVIndex = document.querySelector('#dayFourUVIndex');
 
 const dayFiveDate = document.querySelector('#dayFiveDate');
 const dayFiveIcon = document.querySelector('#dayFiveIcon');
 const dayFiveTemp = document.querySelector('#dayFiveTemp');
 const dayFiveHumidity = document.querySelector('#dayFiveHumidity');
 const dayFiveWindSpeed = document.querySelector('#dayFiveWindSpeed');
-const dayFiveUVIndex = document.querySelector('#dayFiveUVIndex');
+
+const cityName = document.querySelector('#cityName');
+
+const validContainer = document.querySelector('#validContainer');
 
 const searchButton = document.querySelector('#searchButton');
 
@@ -58,22 +56,15 @@ const apiKey = 'bf35508c0373c0cd74246faad2a78d1d';
 function startPage() {
     populateSearchHistory();
     console.log('hello');
-}
-
-function loadSeattle() {
-
-}
-
-//waiting on API to work... check tomorrow
-function populateWeather() {
-    let city = searchedCity.value.trim();
-    let apiKey = 'bf35508c0373c0cd74246faad2a78d1d';
-    let url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
+    let url = `https://api.openweathermap.org/data/2.5/forecast?q=Bellevue&appid=${apiKey}`;
     fetch(url)
         .then(function (response) {
             return response.json();
         }).then(function (json) {
             console.log(json);
+            let thisCity = json.city.name;
+
+            cityName.innerHTML = thisCity;
 
             let imageZeroId = json.list[0].weather[0].icon;
             let imageOneId = json.list[7].weather[0].icon;
@@ -108,10 +99,10 @@ function populateWeather() {
             let dateFour = json.list[31].dt_txt;
             let dateFive = json.list[39].dt_txt;
 
-            dayTwoDate.innerHTML = `${dateTwo}`;
-            dayThreeDate.innerHTML = `${dateThree}`;
-            dayFourDate.innerHTML = `${dateFour}`;
-            dayFiveDate.innerHTML = `${dateFive}`;
+            dayTwoDate.innerHTML = `${dateTwo} Eastern`;
+            dayThreeDate.innerHTML = `${dateThree} Eastern`;
+            dayFourDate.innerHTML = `${dateFour} Eastern`;
+            dayFiveDate.innerHTML = `${dateFive} Eastern`;
 
             let tempKelvinZero = json.list[0].main.temp;
             let tempKelvinOne = json.list[7].main.temp;
@@ -127,20 +118,152 @@ function populateWeather() {
             let tempFahrenheitFour = convertToFahrenheit(tempKelvinFour);
             let tempFahrenheitFive = convertToFahrenheit(tempKelvinFive);
 
-            dayZeroTemp.innerHTML = `${tempFahrenheitZero}`;
-            dayOneTemp.innerHTML = `${tempFahrenheitOne}`;
-            dayTwoTemp.innerHTML = `${tempFahrenheitTwo}`;
-            dayThreeTemp.innerHTML = `${tempFahrenheitThree}`;
-            dayFourTemp.innerHTML = `${tempFahrenheitFour}`;
-            dayFiveTemp.innerHTML = `${tempFahrenheitFive}`;
+            dayZeroTemp.innerHTML = `${tempFahrenheitZero}&deg;F`;
+            dayOneTemp.innerHTML = `${tempFahrenheitOne}&deg;F`;
+            dayTwoTemp.innerHTML = `${tempFahrenheitTwo}&deg;F`;
+            dayThreeTemp.innerHTML = `${tempFahrenheitThree}&deg;F`;
+            dayFourTemp.innerHTML = `${tempFahrenheitFour}&deg;F`;
+            dayFiveTemp.innerHTML = `${tempFahrenheitFive}&deg;F`;
 
+            let humidityZero = json.list[0].main.humidity;
+            let humidityOne = json.list[7].main.humidity;
+            let humidityTwo = json.list[15].main.humidity;
+            let humidityThree = json.list[23].main.humidity;
+            let humidityFour = json.list[31].main.humidity;
+            let humidityFive = json.list[39].main.humidity;
+
+            dayZeroHumidity.innerHTML = `${humidityZero}% humidity`;
+            dayOneHumidity.innerHTML = `${humidityOne}% humidity`;
+            dayTwoHumidity.innerHTML = `${humidityTwo}% humidity`;
+            dayThreeHumidity.innerHTML = `${humidityThree}% humidity`;
+            dayFourHumidity.innerHTML = `${humidityFour}% humidity`;
+            dayFiveHumidity.innerHTML = `${humidityFive}% humidity`;
+
+            let windSpeedZero = json.list[0].wind.speed;
+            let windSpeedOne = json.list[7].wind.speed;
+            let windSpeedTwo = json.list[15].wind.speed;
+            let windSpeedThree = json.list[23].wind.speed;
+            let windSpeedFour = json.list[31].wind.speed;
+            let windSpeedFive = json.list[39].wind.speed;
+
+            dayZeroWindSpeed.innerHTML = `${windSpeedZero} mph wind`;
+            dayOneWindSpeed.innerHTML = `${windSpeedOne} mph wind`;
+            dayTwoWindSpeed.innerHTML = `${windSpeedTwo} mph wind`;
+            dayThreeWindSpeed.innerHTML = `${windSpeedThree} mph wind`;
+            dayFourWindSpeed.innerHTML = `${windSpeedFour} mph wind`;
+            dayFiveWindSpeed.innerHTML = `${windSpeedFive} mph wind`;
+        });
+
+}
+
+//waiting on API to work... check tomorrow
+function populateWeather() {
+    let city = searchedCity.value.trim();
+    let apiKey = 'bf35508c0373c0cd74246faad2a78d1d';
+    let url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
+    fetch(url)
+        .then(function (response) {
+            return response.json();
+        }).then(function (json) {
+            console.log(json);
+
+            let thisCity = json.city.name;
+
+            cityName.innerHTML = thisCity;
+
+            let imageZeroId = json.list[0].weather[0].icon;
+            let imageOneId = json.list[7].weather[0].icon;
+            let imageTwoId = json.list[15].weather[0].icon;
+            let imageThreeId = json.list[23].weather[0].icon;
+            let imageFourId = json.list[31].weather[0].icon;
+            let imageFiveId = json.list[39].weather[0].icon;
+
+            let descriptionZero = json.list[0].weather[0].description;
+            let descriptionOne = json.list[7].weather[0].description;
+            let descriptionTwo = json.list[15].weather[0].description;
+            let descriptionThree = json.list[23].weather[0].description;
+            let descriptionFour = json.list[31].weather[0].description;
+            let descriptionFive = json.list[39].weather[0].description;
+
+            let imageZeroUrl = `http://openweathermap.org/img/wn/${imageZeroId}@2x.png`;
+            let imageOneUrl = `http://openweathermap.org/img/wn/${imageOneId}@2x.png`;
+            let imageTwoUrl = `http://openweathermap.org/img/wn/${imageTwoId}@2x.png`;
+            let imageThreeUrl = `http://openweathermap.org/img/wn/${imageThreeId}@2x.png`;
+            let imageFourUrl = `http://openweathermap.org/img/wn/${imageFourId}@2x.png`;
+            let imageFiveUrl = `http://openweathermap.org/img/wn/${imageFiveId}@2x.png`;
+
+            dayZeroIcon.innerHTML = `<img src='${imageZeroUrl}' /><p>Expect ${descriptionZero}.</p>`;
+            dayOneIcon.innerHTML = `<img src='${imageOneUrl}' /><p>Expect ${descriptionOne}.</p>`;
+            dayTwoIcon.innerHTML = `<img src='${imageTwoUrl}' /><p>Expect ${descriptionTwo}.</p>`;
+            dayThreeIcon.innerHTML = `<img src='${imageThreeUrl}' /><p>Expect ${descriptionThree}.</p>`;
+            dayFourIcon.innerHTML = `<img src='${imageFourUrl}' /><p>Expect ${descriptionFour}.</p>`;
+            dayFiveIcon.innerHTML = `<img src='${imageFiveUrl}' /><p>Expect ${descriptionFive}.</p>`;
+
+            let dateTwo = json.list[15].dt_txt;
+            let dateThree = json.list[23].dt_txt;
+            let dateFour = json.list[31].dt_txt;
+            let dateFive = json.list[39].dt_txt;
+
+            dayTwoDate.innerHTML = `${dateTwo} Eastern`;
+            dayThreeDate.innerHTML = `${dateThree} Eastern`;
+            dayFourDate.innerHTML = `${dateFour} Eastern`;
+            dayFiveDate.innerHTML = `${dateFive} Eastern`;
+
+            let tempKelvinZero = json.list[0].main.temp;
+            let tempKelvinOne = json.list[7].main.temp;
+            let tempKelvinTwo = json.list[15].main.temp;
+            let tempKelvinThree = json.list[23].main.temp;
+            let tempKelvinFour = json.list[31].main.temp;
+            let tempKelvinFive = json.list[39].main.temp;
+
+            let tempFahrenheitZero = convertToFahrenheit(tempKelvinZero);
+            let tempFahrenheitOne = convertToFahrenheit(tempKelvinOne);
+            let tempFahrenheitTwo = convertToFahrenheit(tempKelvinTwo);
+            let tempFahrenheitThree = convertToFahrenheit(tempKelvinThree);
+            let tempFahrenheitFour = convertToFahrenheit(tempKelvinFour);
+            let tempFahrenheitFive = convertToFahrenheit(tempKelvinFive);
+
+            dayZeroTemp.innerHTML = `${tempFahrenheitZero}&deg;F`;
+            dayOneTemp.innerHTML = `${tempFahrenheitOne}&deg;F`;
+            dayTwoTemp.innerHTML = `${tempFahrenheitTwo}&deg;F`;
+            dayThreeTemp.innerHTML = `${tempFahrenheitThree}&deg;F`;
+            dayFourTemp.innerHTML = `${tempFahrenheitFour}&deg;F`;
+            dayFiveTemp.innerHTML = `${tempFahrenheitFive}&deg;F`;
+
+            let humidityZero = json.list[0].main.humidity;
+            let humidityOne = json.list[7].main.humidity;
+            let humidityTwo = json.list[15].main.humidity;
+            let humidityThree = json.list[23].main.humidity;
+            let humidityFour = json.list[31].main.humidity;
+            let humidityFive = json.list[39].main.humidity;
+
+            dayZeroHumidity.innerHTML = `${humidityZero}% humidity`;
+            dayOneHumidity.innerHTML = `${humidityOne}% humidity`;
+            dayTwoHumidity.innerHTML = `${humidityTwo}% humidity`;
+            dayThreeHumidity.innerHTML = `${humidityThree}% humidity`;
+            dayFourHumidity.innerHTML = `${humidityFour}% humidity`;
+            dayFiveHumidity.innerHTML = `${humidityFive}% humidity`;
+
+            let windSpeedZero = json.list[0].wind.speed;
+            let windSpeedOne = json.list[7].wind.speed;
+            let windSpeedTwo = json.list[15].wind.speed;
+            let windSpeedThree = json.list[23].wind.speed;
+            let windSpeedFour = json.list[31].wind.speed;
+            let windSpeedFive = json.list[39].wind.speed;
+
+            dayZeroWindSpeed.innerHTML = `${windSpeedZero} mph wind`;
+            dayOneWindSpeed.innerHTML = `${windSpeedOne} mph wind`;
+            dayTwoWindSpeed.innerHTML = `${windSpeedTwo} mph wind`;
+            dayThreeWindSpeed.innerHTML = `${windSpeedThree} mph wind`;
+            dayFourWindSpeed.innerHTML = `${windSpeedFour} mph wind`;
+            dayFiveWindSpeed.innerHTML = `${windSpeedFive} mph wind`;
         });
     searchedCity.value = '';
 };
 
 //check previous class work for this
 function convertToFahrenheit(tempKelvin) {
-    let tempFahrenheit = 1.8 * (tempKelvin - 273) + 32
+    let tempFahrenheit = Math.round(1.8 * (tempKelvin - 273) + 32);
     return tempFahrenheit;
 };
 
@@ -171,7 +294,96 @@ function populateSearchHistory() {
                     return response.json();
                 }).then(function (json) {
                     console.log(json);
-                    console.log('this is working!!!!!!!!');
+                    let thisCity = json.city.name;
+
+                    cityName.innerHTML = thisCity;
+
+                    let imageZeroId = json.list[0].weather[0].icon;
+                    let imageOneId = json.list[7].weather[0].icon;
+                    let imageTwoId = json.list[15].weather[0].icon;
+                    let imageThreeId = json.list[23].weather[0].icon;
+                    let imageFourId = json.list[31].weather[0].icon;
+                    let imageFiveId = json.list[39].weather[0].icon;
+
+                    let descriptionZero = json.list[0].weather[0].description;
+                    let descriptionOne = json.list[7].weather[0].description;
+                    let descriptionTwo = json.list[15].weather[0].description;
+                    let descriptionThree = json.list[23].weather[0].description;
+                    let descriptionFour = json.list[31].weather[0].description;
+                    let descriptionFive = json.list[39].weather[0].description;
+
+                    let imageZeroUrl = `http://openweathermap.org/img/wn/${imageZeroId}@2x.png`;
+                    let imageOneUrl = `http://openweathermap.org/img/wn/${imageOneId}@2x.png`;
+                    let imageTwoUrl = `http://openweathermap.org/img/wn/${imageTwoId}@2x.png`;
+                    let imageThreeUrl = `http://openweathermap.org/img/wn/${imageThreeId}@2x.png`;
+                    let imageFourUrl = `http://openweathermap.org/img/wn/${imageFourId}@2x.png`;
+                    let imageFiveUrl = `http://openweathermap.org/img/wn/${imageFiveId}@2x.png`;
+
+                    dayZeroIcon.innerHTML = `<img src='${imageZeroUrl}' /><p>Expect ${descriptionZero}.</p>`;
+                    dayOneIcon.innerHTML = `<img src='${imageOneUrl}' /><p>Expect ${descriptionOne}.</p>`;
+                    dayTwoIcon.innerHTML = `<img src='${imageTwoUrl}' /><p>Expect ${descriptionTwo}.</p>`;
+                    dayThreeIcon.innerHTML = `<img src='${imageThreeUrl}' /><p>Expect ${descriptionThree}.</p>`;
+                    dayFourIcon.innerHTML = `<img src='${imageFourUrl}' /><p>Expect ${descriptionFour}.</p>`;
+                    dayFiveIcon.innerHTML = `<img src='${imageFiveUrl}' /><p>Expect ${descriptionFive}.</p>`;
+
+                    let dateTwo = json.list[15].dt_txt;
+                    let dateThree = json.list[23].dt_txt;
+                    let dateFour = json.list[31].dt_txt;
+                    let dateFive = json.list[39].dt_txt;
+
+                    dayTwoDate.innerHTML = `${dateTwo} Eastern`;
+                    dayThreeDate.innerHTML = `${dateThree} Eastern`;
+                    dayFourDate.innerHTML = `${dateFour} Eastern`;
+                    dayFiveDate.innerHTML = `${dateFive} Eastern`;
+
+                    let tempKelvinZero = json.list[0].main.temp;
+                    let tempKelvinOne = json.list[7].main.temp;
+                    let tempKelvinTwo = json.list[15].main.temp;
+                    let tempKelvinThree = json.list[23].main.temp;
+                    let tempKelvinFour = json.list[31].main.temp;
+                    let tempKelvinFive = json.list[39].main.temp;
+
+                    let tempFahrenheitZero = convertToFahrenheit(tempKelvinZero);
+                    let tempFahrenheitOne = convertToFahrenheit(tempKelvinOne);
+                    let tempFahrenheitTwo = convertToFahrenheit(tempKelvinTwo);
+                    let tempFahrenheitThree = convertToFahrenheit(tempKelvinThree);
+                    let tempFahrenheitFour = convertToFahrenheit(tempKelvinFour);
+                    let tempFahrenheitFive = convertToFahrenheit(tempKelvinFive);
+
+                    dayZeroTemp.innerHTML = `${tempFahrenheitZero}&deg;F`;
+                    dayOneTemp.innerHTML = `${tempFahrenheitOne}&deg;F`;
+                    dayTwoTemp.innerHTML = `${tempFahrenheitTwo}&deg;F`;
+                    dayThreeTemp.innerHTML = `${tempFahrenheitThree}&deg;F`;
+                    dayFourTemp.innerHTML = `${tempFahrenheitFour}&deg;F`;
+                    dayFiveTemp.innerHTML = `${tempFahrenheitFive}&deg;F`;
+
+                    let humidityZero = json.list[0].main.humidity;
+                    let humidityOne = json.list[7].main.humidity;
+                    let humidityTwo = json.list[15].main.humidity;
+                    let humidityThree = json.list[23].main.humidity;
+                    let humidityFour = json.list[31].main.humidity;
+                    let humidityFive = json.list[39].main.humidity;
+
+                    dayZeroHumidity.innerHTML = `${humidityZero}% humidity`;
+                    dayOneHumidity.innerHTML = `${humidityOne}% humidity`;
+                    dayTwoHumidity.innerHTML = `${humidityTwo}% humidity`;
+                    dayThreeHumidity.innerHTML = `${humidityThree}% humidity`;
+                    dayFourHumidity.innerHTML = `${humidityFour}% humidity`;
+                    dayFiveHumidity.innerHTML = `${humidityFive}% humidity`;
+
+                    let windSpeedZero = json.list[0].wind.speed;
+                    let windSpeedOne = json.list[7].wind.speed;
+                    let windSpeedTwo = json.list[15].wind.speed;
+                    let windSpeedThree = json.list[23].wind.speed;
+                    let windSpeedFour = json.list[31].wind.speed;
+                    let windSpeedFive = json.list[39].wind.speed;
+
+                    dayZeroWindSpeed.innerHTML = `${windSpeedZero} mph wind`;
+                    dayOneWindSpeed.innerHTML = `${windSpeedOne} mph wind`;
+                    dayTwoWindSpeed.innerHTML = `${windSpeedTwo} mph wind`;
+                    dayThreeWindSpeed.innerHTML = `${windSpeedThree} mph wind`;
+                    dayFourWindSpeed.innerHTML = `${windSpeedFour} mph wind`;
+                    dayFiveWindSpeed.innerHTML = `${windSpeedFive} mph wind`;
                 });
 
         })
@@ -189,9 +401,17 @@ function clearSearchHistory() {
 
 searchButton.addEventListener('click', function (e) {
     e.preventDefault();
-    addToSearchHistory();
-    populateWeather();
-    populateSearchHistory();
+    if (searchedCity.value !== '') {
+        addToSearchHistory();
+        populateWeather();
+        populateSearchHistory();
+    } else {
+        validContainer.textContent = 'Please input a city or zipcode.';
+        setTimeout(function () {
+            validContainer.textContent = '';
+        }, 2500);
+    }
+
 });
 
 clearHistoryButton.addEventListener('click', function (e) {
